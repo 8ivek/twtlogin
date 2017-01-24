0d5693eef9a1ee704134fcaed9803c64bb33c66d
Bivek <bivek_j@yahoo.com>
2017-01-23 23:35:12 -0500

    twitter first commit

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+define('CONSUMER_KEY', 'MCIRT0gR9wYSDkbnCz8eUfp8z');
+define('CONSUMER_SECRET', 'nEE4Lxc7fKDToiywBgriEIFBzUBLGoUaSXk3WZltQTxtWZVcQU');
+define('OAUTH_CALLBACK', 'http://localhost/twtlogin/process.php');
+?>
